f12b599c6e4fefd77939fdfd5a4046f775b108a6
fxivan <almendraivan210814@gmail.com>
2025-12-07 12:11:49 -0300

    Actividad 3 Finalizada

--- EjercicioPolimorfismoHerencia/Program.cs
+++ EjercicioPolimorfismoHerencia/Program.cs
@@ -151,6 +151,72 @@ public class Pajaro : Animal
     }
 }
 */
+
+/*
+Actividad 3: Polimorfismo aplicado a lógica real
+
+Objetivo: entender para qué sirve realmente.
+
+1.Crea una función:
+   public void AtenderAnimal(Animal a)
+   Dentro llama:
+    - a.Comer()
+    - a.HacerSonido()
+
+2.Pásale distintos animales.
+
+Qué aprendes:
+- Un método puede recibir cualquier derivado sin que tú lo sepas de antemano.
+- Polimorfismo = código más genérico y flexible.
+ */
+
+public class Animal
+{
+    public virtual string Comer()
+    {
+        return "El animal esta comiendo";
+    }
+
+    public virtual string HacerSonido()
+    {
+        return "El animal esta haciendo sonido";
+    }
+
+    public static void AtenderAnimal(Animal a)
+    {
+        string salidaComer = a.Comer();
+        string salidaSonido = a.HacerSonido();
+        Console.WriteLine(salidaComer + "\n" + salidaSonido);
+    }
+}
+
+public class Perro : Animal
+{
+    public override string Comer()
+    {
+        return "El perro esta comiendo";
+    }
+
+    public override string HacerSonido()
+    {
+        return "El perro esta ladrando";
+    }
+
+}
+
+public class Gato : Animal
+{
+    public override string Comer()
+    {
+        return "El gato esta comiendo un pescado";
+    }
+
+    public override string HacerSonido()
+    {
+        return "El gato esta maullando";
+    }
+}
+
 class Program
 {
     static void Main()
@@ -197,5 +263,10 @@ class Program
         }
         */
 
+        Animal perro = new Perro();
+        Animal gato = new Gato();
+
+        Animal.AtenderAnimal(perro);
+        Animal.AtenderAnimal(gato);
     }
-}
+};
